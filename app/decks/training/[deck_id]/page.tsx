f132675ec9a3d3d99ebf1f801be074130c08566a
totato4 <v4.97@list.ru>
2025-04-23 09:06:52 +0300

    training-card component

--- app/decks/training/[deck_id]/page.tsx
+++ app/decks/training/[deck_id]/page.tsx
@@ -1,13 +1,17 @@
+import TrainingCard from "@/app/components/training-card";
 import { getDeckById } from "@/app/lib/actions";
+import { getDeckCards } from "@/app/lib/data";
 
 export default async function Page(props: {
   params: Promise<{ deck_id: string; title: string }>;
 }) {
   const { deck_id } = await props.params;
   const deck = await getDeckById(deck_id);
+  const cards = await getDeckCards(deck_id);
   return (
     <div>
       <h1>{deck.title}</h1>
+      <TrainingCard cards={cards} />
     </div>
   );
 }
