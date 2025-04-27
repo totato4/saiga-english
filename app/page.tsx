d21694de1ed057330b7c9c2a535957fb7d5c5175
Capibara888 <git.san@bk.ru>
2025-04-27 16:10:04 +0300

    create colors for theming

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,11 @@ import CreateModal from "./ui/deck/create-modal";
 export default function Page() {
   return (
     <main className="flex flex-col">
-      <h1 className=" mx-auto pt-10  bg-bg-light dark:bg-bg-dark text-text-light dark:text-text-dark ">
+      <h1
+        className=" mx-auto pt-10  
+      
+      "
+      >
         Saiga English
       </h1>
       <h2 className="text-display mx-auto ">
